3cbf3a1df37c520c526dd0ddd7c1404b0eb6295a
Jake <ezu7mt@virginia.edu>
2024-11-03 17:19:26 -0500

    'new' tag trigger

--- P4/trigger.sql
+++ P4/trigger.sql
@@ -1,1 +1,26 @@
--- create 1 trigger associated with any type of action between the referenced tables (primary-foreign key relationship tables)
+
+--//Adds a 'New' tag to an org when it is created. Nice for users to be able to look for new orgs. 
+
+CREATE TRIGGER NewOrgTag
+ON organization
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @NewOrgId INT
+    DECLARE @NewTagId INT
+
+    SELECT @NewOrgId = org_id FROM inserted
+
+    BEGIN
+        -- Gets max tag id from tag table, adds 1 to it to make it unique
+        SELECT @NewTagId = MAX(tag_id) + 1 FROM tag
+        
+        -- Create 'New' tag with the new tag id
+        INSERT INTO tag (tag_id, name)
+        VALUES (@NewTagId, 'New')
+    END
+
+    INSERT INTO organization_tag (org_id, tag_id)
+    VALUES (@NewOrgId, @NewTagId)
+END
+GO
