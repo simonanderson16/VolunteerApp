e544bc199de417e73b0db9889d78959223ce7653
Ethan Haller <ethanhaller02@gmail.com>
2024-11-06 15:50:09 -0500

    Add 3 non-clustered indexes

--- P4/indexes.sql
+++ P4/indexes.sql
@@ -1,1 +1,8 @@
--- create 3 non-clustered indexes on tables
+-- Non-clustered index to more efficiently lookup users by their email
+CREATE NONCLUSTERED INDEX Idx_person_email ON person (email);
+
+-- Non-clustered index to more efficiently join person onto sign_up to view names of people who have signed up
+CREATE NONCLUSTERED INDEX Idx_sign_up_person ON sign_up (person_id)
+
+-- Non-clustered index to more efficiently join organization information with event information
+CREATE NONCLUSTERED INDEX Idx_event_organization ON [event] (org_id)
