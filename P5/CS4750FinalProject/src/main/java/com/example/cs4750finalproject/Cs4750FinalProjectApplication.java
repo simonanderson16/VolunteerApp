f7e91fcf07f50eaa3919c9b9b546bf902be5e5fb
simonanderson16 <uhx8bu@virginia.edu>
2024-11-20 17:40:01 -0500

    initialized springboot project

---
+++ P5/CS4750FinalProject/src/main/java/com/example/cs4750finalproject/Cs4750FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs4750finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4750FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs4750FinalProjectApplication.class, args);
+    }
+
+}
