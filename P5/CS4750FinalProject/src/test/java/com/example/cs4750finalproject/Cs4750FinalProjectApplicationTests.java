4219d6d961845775e6828232dda9d2932b4db423
simonanderson16 <uhx8bu@virginia.edu>
2024-11-21 22:31:04 -0500

    reinitialized springboot app

---
+++ P5/CS4750FinalProject/src/test/java/com/example/cs4750finalproject/Cs4750FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs4750finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4750FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
