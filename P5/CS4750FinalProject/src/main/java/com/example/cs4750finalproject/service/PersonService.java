b633f1aeb478511be674a6eb6903bebe2ab1b2c4
Grant Sweeney <grantsweeney02@gmail.com>
2024-11-25 12:11:13 -0500

    Update PersonService.java

--- P5/CS4750FinalProject/src/main/java/com/example/cs4750finalproject/service/PersonService.java
+++ P5/CS4750FinalProject/src/main/java/com/example/cs4750finalproject/service/PersonService.java
@@ -2,6 +2,7 @@ package com.example.cs4750finalproject.service;
 
 import com.example.cs4750finalproject.model.EmergencyContact;
 import com.example.cs4750finalproject.model.Person;
+import com.example.cs4750finalproject.repository.EmergencyContactRepository;
 import com.example.cs4750finalproject.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private EmergencyContactRepository emergencyContactRepository;
+
     public List<Person> getAllPersons() {
         return personRepository.findAll();
     }
@@ -24,21 +28,27 @@ public class PersonService {
     }
 
     public Person createPerson(Person person) {
+        EmergencyContact contact = emergencyContactRepository.findById(person.getEmergencyContact().getContactId())
+                .orElseThrow(() -> new RuntimeException("EmergencyContact not found"));
+
+        person.setEmergencyContact(contact);
+
         return personRepository.save(person);
     }
 
     public Person updatePerson(Integer id, Person updatedPerson) {
-        Optional<Person> existingPerson = personRepository.findById(id);
-        if (existingPerson.isPresent()) {
-            Person person = existingPerson.get();
-            person.setName(updatedPerson.getName());
-            person.setEmail(updatedPerson.getEmail());
-            person.setPassword(updatedPerson.getPassword());
-            person.setEmergencyContact(updatedPerson.getEmergencyContact());
-            return personRepository.save(person);
-        } else {
+        Optional<Person> existingPersonOptional = personRepository.findById(id);
+        if (existingPersonOptional.isEmpty()) {
             throw new RuntimeException("Person not found");
         }
+        Person existingPerson = existingPersonOptional.get();
+        existingPerson.setName(updatedPerson.getName());
+        existingPerson.setEmail(updatedPerson.getEmail());
+        existingPerson.setPassword(updatedPerson.getPassword());
+        EmergencyContact contact = emergencyContactRepository.findById(updatedPerson.getEmergencyContact().getContactId())
+                .orElseThrow(() -> new RuntimeException("EmergencyContact not found"));
+        existingPerson.setEmergencyContact(contact);
+        return personRepository.save(existingPerson);
     }
 
     public void deletePerson(Integer id) {
